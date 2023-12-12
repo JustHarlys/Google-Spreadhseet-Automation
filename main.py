0886f13ea41cd36a5c64b185d26b41c69dc85d5c
harly <harlysmc@gmail.com>
2023-12-12 14:53:14 -0400

    Finalizada la parte de ver entradas

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file" , "https://www.googleapis.com/auth/drive"]
-
 creds = ServiceAccountCredentials.from_json_keyfile_name('falcon.json', scope)
-
 client = gspread.authorize(creds)
-
 sheet = client.open('Prueba').sheet1
 
-# access data
-#print(sheet.get_all_records())
-#print(sheet.row_values(3))
-#print(sheet.col_values(1))
-# print(sheet.cell(2,1).value)
 
-# insert data
-# sheet.insert_row(['Cats', 'Dogs'], 3)
+const = 0
 
-# delete row
-# sheet.delete_row(3)
+while True:
+    print("""
+Que vas a hacer?
 
-#update cell
-# sheet.update_cell(2,1, 'Jejeje')
+0) Ver
+1) Insertar
+2) Eliminar
+3) Actualizar
+4) Salir
+""")
 
-# Look for a value
-# cell = sheet.find('Jejeje')
-# print(cell.value)
-# print(cell.row)
-# print(cell.col)
+    const = int(input("Elige una opción: "))
+
+    if const == 0:
+        const_0 = 0
+        while True:
+            print(""" 
+                  
+'--------------------------'
+                
+Que quieres ver?
+                  
+0) Todas las entradas?
+1) Valores de una fila? (Especificar cual)
+2) Valores de una columna? (Especificar cual)
+3) Valores de celda? (Especificar cuales i.e (2,1) F=2, C=1)
+4) Salir                    
+                    """)
+            
+            const_0 = int(input("Elige una opción: "))
+
+            if const_0 == 0:
+                print(sheet.get_all_records())
+                break
+            elif const_0 == 1:
+                print(sheet.row_values(int(input("Que fila quieres ver?: "))))
+                break
+            elif const_0 == 2:
+                print(sheet.col_values(int(input("Que columna deseas ver?: "))))
+                break
+            elif const_0 == 3:
+                print(sheet.cell(int(input("Que celda deseas ver? F,C"))))
+                break
+            elif const_0 == 4:
+                break
+            else:
+                print('Ingresa una opción válida, por favor')
+            
+                
 
 
-# Look for a value with a 'for' loop  - Brings every related value
-# cell_list = sheet.find('Jejeje')
-# for cell in cell_list:
-    # print(cell.value)
-    # print(cell.row)
-    # print(cell.col)
